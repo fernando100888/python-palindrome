dae8d41dad74f46dfef733543a728e39ddf8ea37
fernando <fernando.cr100888@gmail.com>
2022-08-03 17:01:02 -0400

    nested function and closure definition

---
+++ nested_function/nested_function.py
@@ -0,0 +1,24 @@
+#nested functions are functions that are created within another function
+#1st example
+def main():
+    a = 1
+    def nested():
+        print(a)
+    nested()
+main() # 1
+#I define the function main that contains a value a=1 and it also contains another function called nested that prints the value of a and displays it. When calling the main function, it will give us 1, which is the value of a that was printed by the nested function inside the main function.
+
+
+
+
+
+#2nd example
+def main():
+    a=1
+    def nested():
+        print(a)
+    return nested
+my_function = main()
+my_function() # 1
+# in this example instead of executing nested, return nested. By doing this we can create variables, such as my_function, to which we assign the result of executing the main function. When this happens, the variable a=1 is created, the nested function is created, which will print the value of a, and nested is returned, so my_function will contain a function of type nested that will contain everything what the function contains nested inside the main function, in this case 1. Since my_function contains an object of type function, we can execute it using the parentheses of a function. This technique in which values that are in higher scopes are remembered is called closures.
+
